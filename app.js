8568ab440115057f37aade8e0fc31860c4ee26c3
Nimmana Shanmukha Sai <89311670+shanmukhasaisai@users.noreply.github.com>
2023-04-26 17:32:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent =
-	"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-	"Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-	"Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const WelcomeBlog ="Welcome to my Blog website you can write a blog and post it by adding the /compose to the url thank you hope it is useful";
+const AboutMe = "Hello everyone, my name is Nimmana Shanmukha Sai. I am from Hyderabad, Telangana, India. I have created this website as a project here you can write a blog with a title and content in it and post the blog. I currently pursuing BTech 3rd year at CVR College of Engineering. hope you would like this website. ";
+const ContactMe =
+	"This is the contact page … My name is shanmukha sai. This is my linked in profile https://www.linkedin.com/in/shanmukha-sai-68ab53213/ this is my github profile: https://github.com/shanmukhasaisai and this is my email id: shanmukhasaisai09@gmail.com feel free to send a message";
 const app = express();
 
 app.set("view engine", "ejs");
